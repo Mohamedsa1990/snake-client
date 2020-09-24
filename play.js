9b483c7d0405b0ff231630e3ac4ee72848c71c27
mohamedsa1990 <mohamedsa1990@gmail.com>
2020-09-24 01:41:44 +0000

    set setup input in separate file

--- play.js
+++ play.js
@@ -1,27 +1,8 @@
 
-
+const setupInput = require('./input');
 const connect = require('./client');
 
 console.log('Connecting ...');
 connect();
+setupInput();
 
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', handleUserInput)
-  return stdin;
-}
-
-const handleUserInput = function(data) {
-  if (data === '\u0003') {
-    process.exit();
-  }
-}
-
-setupInput()
